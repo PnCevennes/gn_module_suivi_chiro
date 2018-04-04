f000f5ecac7914ce7318a749e1fb007f479893e6
amandine-sahl <amandine.sahl@gmail.com>
2018-04-04 09:31:07 +0200

    Modification du format de retour des sites pour qu'ils soient compatibles avec le front-end

--- backend/routes/site.py
+++ backend/routes/site.py
@@ -2,21 +2,21 @@ from flask import request
 
 from geonature.utils.env import DB
 from geonature.utils.utilssqlalchemy import json_resp
-from pypnnomenclature.models import TNomenclatures
-from pypnnomenclature import repository
 
 from ..blueprint import blueprint
 from ..models.site import InfoSite
 
-
+import geojson
+from geoalchemy2.shape import to_shape
 
 
 def _format_site_data(data):
     '''
     Procédure de sérialisation non récursive des modèles
     '''
-    base = data.base_site.as_geofeature('geom', 'id_base_site')
-
+    base = data.base_site.as_dict(recursif=False)
+    geometry = geojson.Feature(geometry=to_shape(getattr(data.base_site, 'geom')))
+    base['geom'] = [geometry['geometry']['coordinates']]
     result = data.as_dict(recursif=False)
     result['menaces_ids'] = [
             menace.id_nomenclature_menaces
@@ -24,7 +24,7 @@ def _format_site_data(data):
     result['amenagements_ids'] = [
             amenagement.id_nomenclature_amenagement
             for amenagement in data.amenagements_ids]
-    base['properties'].update(result)
+    base.update(result)
     return base
 
 
@@ -36,11 +36,13 @@ def get_sites_chiro():
     '''
     limit = int(request.args.get('limit', 10))
     offset = int(request.args.get('offset', 1))
-    results = (DB.session.query(InfoSite)
-            .order_by(InfoSite.id_site_infos)
-            .limit(limit)
-            .offset((offset-1)*limit)
-            .all())
+    results = (
+        DB.session.query(InfoSite)
+        .order_by(InfoSite.id_site_infos)
+        .limit(limit)
+        .offset((offset-1)*limit)
+        .all()
+    )
     return list(map(_format_site_data, results))
 
 
@@ -50,7 +52,7 @@ def get_one_site_chiro(id_site):
     '''
     Retourne le site chiro identifié par `id_site`
     '''
-    result = DB.session.query(InfoSite).get(id_site)
+    result = DB.session.query(InfoSite).filter_by(id_base_site=id_site).one()
     if result:
         return _format_site_data(result)
     return {'err': 'site introuvable', 'id_site': id_site}, 404
