291e3c5deb81aaebf7b75a0f44c4024ad125bea2
amandine-sahl <amandine.sahl@gmail.com>
2018-06-06 12:53:02 +0200

    Finalisation fonction blueprint

--- backend/blueprint.py
+++ backend/blueprint.py
@@ -7,7 +7,11 @@ from flask import Blueprint, request
 from geonature.utils.env import DB
 from geonature.utils.utilssqlalchemy import json_resp
 
+from geonature.core.gn_monitoring.models import TBaseVisits
 from .models.site import InfoSite
+from .models.contact_taxon import ContactTaxon
+from .models.biometrie import Biometrie
+
 
 ID_APP = 101 # TODO récupérer l'identifiant d'application par le front
 
@@ -18,15 +22,52 @@ blueprint = Blueprint('gn_module_suivi_chiro', __name__)
 def load_site(id_site):
     result = DB.session.query(InfoSite).filter_by(id_base_site=id_site).one()
     return {
-            'id': id_site,
-            'link': '#/suivi_chiro/site/%s' % id_site,
-            'label': result.base_site.base_site_name
-            }
+        'id': id_site,
+        'link': '#/suivi_chiro/site/%s' % id_site,
+        'label': result.base_site.base_site_name
+    }
 
 
-@blueprint.route('/')
-def gn_module_suivi_chiro_index():
-    return 'Ca marche yep yep'
+def load_visite(id_visite):
+    result = DB.session.query(
+        TBaseVisits
+    ).filter_by(id_base_visit=id_visite).one()
+
+    bread = [load_site(result.id_base_site)]
+
+    bread.append({
+        'id': id_visite,
+        'link': '#/suivi_chiro/observation/%s' % id_visite,
+        'label': str(result.visit_date)
+    })
+    return bread
+
+
+def load_taxon(id_taxon):
+    result = DB.session.query(
+        ContactTaxon
+    ).filter_by(id_contact_taxon=id_taxon).one()
+
+    bread = load_visite(result.id_base_visit)
+    bread.append({
+        'id': id_taxon,
+        'link': '#/suivi_chiro/taxons/%s' % id_taxon,
+        'label': str(result.nom_complet)
+    })
+    return bread
+
+def load_biometrie(id_biometrie):
+    result = DB.session.query(
+        Biometrie
+    ).filter_by(id_biometrie=id_biometrie).one()
+
+    bread = load_taxon(result.id_contact_taxon)
+    bread.append({
+        'id': id_biometrie,
+        'link': '#/suivi_chiro/biometrie/%s' % id_biometrie,
+        'label': str(result.id_biometrie)
+    })
+    return bread
 
 
 @blueprint.route('/config/breadcrumb')
@@ -37,16 +78,28 @@ def breadcrumb():
 
     out = [{'id': None, 'link': '#/suivi_chiro/site', 'label': 'Sites'}]
 
-    if view == 'site':
-        if id_obj:
+    if id_obj:
+        if view == 'site':
             out.append(load_site(id_obj))
+        if view == 'observation':
+            breads = load_visite(id_obj)
+            for bread in breads:
+                out.append(bread)
+        if view == 'taxons':
+            breads = load_taxon(id_obj)
+            for bread in breads:
+                out.append(bread)
+        if view == 'biometrie':
+            breads = load_biometrie(id_obj)
+            for bread in breads:
+                out.append(bread)
 
     return out
 
 
 from .routes import (
-        site,
-        visite,
-        contact_taxon,
-        biometrie
-        )
+    site,
+    visite,
+    contact_taxon,
+    biometrie
+)
