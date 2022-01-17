1be46fdef694e105e1ebf1d9fc0b6bf3c861cb32
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2022-01-17 10:44:52 +0100

    Rappatriment de la route générique config qui jusqu'à présent n'est utilisée que par ce module

--- backend/blueprint.py
+++ backend/blueprint.py
@@ -5,13 +5,22 @@ point d'entrée du module chiro
 from sqlalchemy.orm.exc import NoResultFound
 
 
-from flask import Blueprint, request, current_app
+from flask import Blueprint, request, current_app, jsonify
 
 from geonature.utils.env import DB
 from geonature.utils.utilssqlalchemy import json_resp
 
 from geonature.core.gn_monitoring.models import TBaseVisits
 from .models.models import InfoSite, ContactTaxon, Biometrie
+
+
+
+import os 
+ 
+from .utils.config_manager import generate_config
+
+from geonature.core.gn_permissions import decorators as permissions
+ 
 
 blueprint = Blueprint('gn_module_suivi_chiro', __name__)
 
@@ -109,9 +118,43 @@ def breadcrumb():
     return out
 
 
+
+
 from .routes import (
     site,
     visite,
     contact_taxon,
     biometrie
 )
+
+
+# PATCH lié à la suppression de la route config du coeur de GeoNature et à son déplacement dans le module chiro
+@blueprint.route("/config", methods=["GET"])
+@permissions.check_cruved_scope("R", False, module_code="SUIVIS")
+def get_config():
+    """
+        Parse and return configuration files as toml
+        
+        Définition de routes "génériques"  c-a-d pouvant servir à tous modules
+        Initialement situé dans le coeur de GeoNature mais considéré comme trop spécifique
+            la route et les méthodes afférentes ont été déplacées dans le module chiro
+            qui est le seul à ma connaissance à utiliser ce mécanisme
+    """
+    app_name = request.args.get("app", "base_app")
+    vue_name = request.args.getlist("vue")
+
+    base_path = os.path.abspath(os.path.join(current_app.static_folder, "configs"))
+    conf_path = os.path.abspath(
+            os.path.join(base_path, app_name, *vue_name)
+    )
+    # test : file inside config folder
+    if not conf_path.startswith(base_path):
+        return "Not a valid config path", 404
+
+    if not vue_name:
+        vue_name = ["default"]
+    filename = "{}.toml".format(
+        conf_path
+    )
+    config_file = generate_config(filename)
+    return jsonify(config_file)
